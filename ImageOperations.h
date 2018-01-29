dda90bec372a811f24fd9e4b07b4b25091b7f4de
xsl <375774600@qq.com>
2018-01-29 16:47:13 +0800

    firstCommit1

--- ImageOperations.h
+++ ImageOperations.h
@@ -9,16 +9,7 @@ public:
 	~ImageOperations();
 
 public:
-	//图片缩放
-	void ZoomImage(cv::Mat& image);
-	//图片移动
-	void MoiveImage(cv::Mat& image);
-	//Camma矫正
-	cv::Mat GammaTransform(cv::Mat& image, float kFactor);
-	//分段灰度拉伸
-	cv::Mat contrastStetch(cv::Mat& grayImage);
-	//灰度分层
-	cv::Mat Graylayered(cv::Mat& image);
+
 };
 #endif
 
